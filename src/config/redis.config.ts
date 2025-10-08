172f14c396c145ec01db33cfbe0a5023b55fe30d
shebin ks <shebinks01@gmail.com>
2025-10-08 12:36:48 +0530

    implement redis url

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,15 +1,20 @@
 import Redis from "ioredis";
 
+let redis: Redis;
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: Number(process.env.REDIS_PORT || 6379),
-    password: process.env.REDIS_PASSWORD || undefined,
-    db: 0,
-    connectTimeout: 1000,
-    maxRetriesPerRequest: 0,
-    retryStrategy: () => null,
-});
+if (process.env.REDIS_URL) {
+    redis = new Redis(process.env.REDIS_URL);
+} else {
+    redis = new Redis({
+        host: process.env.REDIS_HOST || "127.0.0.1",
+        port: Number(process.env.REDIS_PORT || 6379),
+        password: process.env.REDIS_PASSWORD || undefined,
+        db: 0,
+        connectTimeout: 1000,
+        maxRetriesPerRequest: 0,
+        retryStrategy: () => null,
+    });
+}
 
 redis.on("connect", () => {
     console.log("Connected to Redis");
