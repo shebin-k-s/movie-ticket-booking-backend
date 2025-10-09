203145847ac11c57357d35f1510a4c837e513bb7
shebin ks <shebinks01@gmail.com>
2025-10-09 12:28:27 +0530

    add url in datasource

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,12 +12,12 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    // url: process.env.DATABASE_URL,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL,
+    // host: process.env.DB_HOST,
+    // port: parseInt(process.env.DB_PORT),
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     ssl: {
